94e43b6fb0c7ea7353660075af4cf14db65ac813
Dani-Rajpoot <103390848+Dani-Rajpoot@users.noreply.github.com>
2022-07-22 20:09:20 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { fetchPosts } from "./features/posts/postsSlice";
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchPosts());
+store.dispatch(fetchUsers());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
